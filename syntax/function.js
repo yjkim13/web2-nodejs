fe002b9a19570c1ae377d1a91d0be3a8358cb549
yjkim13 <sk940913@naver.com>
2021-08-19 20:28:22 +0900

    JS 함수 공부

---
+++ syntax/function.js
@@ -0,0 +1,22 @@
+f123();
+console.log('A');
+console.log('B');
+console.log('C');
+f123();
+console.log('A');
+console.log('Z');
+console.log('X');
+f123();
+console.log('K');
+console.log('F');
+console.log('C');
+console.log('F');
+console.log('Q');
+f123();
+
+function f123(){
+  console.log(1);
+  console.log(2);
+  console.log(3);
+  console.log(4);
+}
