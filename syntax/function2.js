5a8b63fadc0b31699f0778aed1203bff83b7d304
yjkim13 <sk940913@naver.com>
2021-08-19 20:28:36 +0900

    JS 함수 공부 2

---
+++ syntax/function2.js
@@ -0,0 +1,11 @@
+console.log(Math.round(1.6)); //2
+console.log(Math.round(1.4)); //1
+
+
+function sum(first, second){ // parameter
+console.log('1');
+return first+second;
+console.log('2');
+}
+
+console.log(sum(2,4)); // argument
