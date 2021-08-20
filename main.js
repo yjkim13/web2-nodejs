68104c0a6b9e6153390ee3e90d83d36e027475d2
yjkim13 <sk940913@naver.com>
2021-08-20 18:33:12 +0900

    수정할 정보 전송

--- main.js
+++ main.js
@@ -66,7 +66,7 @@ var app = http.createServer(function(request,response){
         var title = `WEB - create`
         var list = templateList(filelist);
         var template = templateHTML(title,list,`
-          <form action="http://localhost:3000/create_process" method="post">
+          <form action="/create_process" method="post">
           <p><input type="text" name="title" placeholder="title"></p>
           <p>
             <textarea name="description"  placeholder="description"></textarea>
@@ -96,6 +96,27 @@ var app = http.createServer(function(request,response){
             //페이지를 다른 곳으로 Redirection
             response.end('success');
           })
+      });
+    } else if (pathname === '/update') {
+      fs.readdir(`./data`,function(err, filelist){
+        fs.readFile(`data/${queryData.id}`,`utf8`,function(err,description){
+            var title = queryData.id;
+            var list = templateList(filelist);
+            var template =templateHTML(title,list,
+              `<form action="/update_process" method="post">
+              <input type="hidden" name="id" value="${title}">
+              <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+              <p>
+                <textarea name="description"  placeholder="description">${description}</textarea>
+              <p>
+                <input type="submit">
+              </p>
+            </form>
+              `,
+              `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
+          response.writeHead(200);
+          response.end(template);
+        });
       });
     } else {
       response.writeHead(404);
